584a90e697a8b9388d6902fe02d5cae1bb0e950f
Mark Keen <markchugnut@ntlworld.com>
2015-07-02 19:18:03 +0100

    Google drive sync in place - need to sort multiple device syncing (need to force google api to sync immediately, rather than queue, then sync)

--- app/src/main/java/com/treefrogapps/TaDo/Constants.java
+++ app/src/main/java/com/treefrogapps/TaDo/Constants.java
@@ -4,98 +4,101 @@ package com.treefrogapps.TaDo;
 public class Constants {
 
     // Shared Preferences constant
-    protected final static String TADO_PREFERENCES = "com.treefrogapps.TaDo.Preferences";
+    protected static final String TADO_PREFERENCES = "com.treefrogapps.TaDo.Preferences";
+    
+    // Sync Fragment
+    protected static final int REQUEST_CODE_RESOLUTION = 10;
 
     // splash screen visibility - place in shared prefs
-    protected final static String SPLASH_SCREEN_VISIBILITY = "com.treefrogapps.TaDo.Visibility";
-    protected final static int SPLASH_SCREEN_OFF = 0;
+    protected static final String SPLASH_SCREEN_VISIBILITY = "com.treefrogapps.TaDo.Visibility";
+    protected static final int SPLASH_SCREEN_OFF = 0;
 
-    protected final static String SWIPE_SPLASH_SCREEN_VISIBILITY = "com.treefrogapps.TaDo.SwipeVisibility";
-    protected final static int SWIPE_SPLASH_SCREEN_OFF = 0;
+    protected static final String SWIPE_SPLASH_SCREEN_VISIBILITY = "com.treefrogapps.TaDo.SwipeVisibility";
+    protected static final int SWIPE_SPLASH_SCREEN_OFF = 0;
 
     // Database constants
-    protected final static String DATABASE_NAME = "main_database.db";
-    protected final static int DATABASE_VERSION = 1;
+    protected static final String DATABASE_NAME = "main_database.db";
+    protected static final int DATABASE_VERSION = 1;
     // to allow on cascade delete
-    protected final static String FOREIGN_KEY_CASCADE = "PRAGMA foreign_keys = ON;";
-    protected final static String DROP_TITLES_TABLE = "DROP TABLE IF EXISTS titles_list;";
-    protected final static String DROP_ITEMS_TABLE = "DROP TABLE IF EXISTS items_list;";
+    protected static final String FOREIGN_KEY_CASCADE = "PRAGMA foreign_keys = ON;";
+    protected static final String DROP_TITLES_TABLE = "DROP TABLE IF EXISTS titles_list;";
+    protected static final String DROP_ITEMS_TABLE = "DROP TABLE IF EXISTS items_list;";
 
 
     // Titles table
-    protected final static String TITLES_TABLE =
+    protected static final String TITLES_TABLE =
             "CREATE TABLE IF NOT EXISTS titles_list " +
                     "(title_id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR, date DATETIME);";
 
-    protected final static String TITLES_LIST = "titles_list";
-    protected final static String TITLE_ID = "title_id";
-    protected final static String TITLE = "title";
-    protected final static String TITLE_DATETIME = "date";
+    protected static final String TITLES_LIST = "titles_list";
+    protected static final String TITLE_ID = "title_id";
+    protected static final String TITLE = "title";
+    protected static final String TITLE_DATETIME = "date";
 
 
     // Items Table
-    protected final static String ITEMS_TABLE =
+    protected static final String ITEMS_TABLE =
             "CREATE TABLE IF NOT EXISTS items_list " +
                     "(item_id INTEGER PRIMARY KEY AUTOINCREMENT," +
                     " item VARCHAR, item_detail VARCHAR, title_id INTEGER NOT NULL," +
                     " duration TIME, date DATETIME, item_done CHAR, priority CHAR," +
                     " FOREIGN KEY (title_id) REFERENCES titles_list (title_id) ON DELETE CASCADE);";
 
-    protected final static String ITEMS_LIST = "items_list";
-    protected final static String ITEMS_ID = "item_id";
-    protected final static String ITEM = "item";
-    protected final static String ITEM_DETAIL = "item_detail";
-    protected final static String ITEM_DURATION = "duration";
-    protected final static String ITEM_DATETIME = "date";
-    protected final static String ITEM_DONE_COLUMN = "item_done";
-    protected final static String ITEM_DONE = "Y";
-    protected final static String ITEM_NOT_DONE = "N";
-    protected final static String ITEM_PRIORITY = "priority";
-    protected final static String ITEM_PRIORITY_HIGH = "H";
-    protected final static String ITEM_PRIORITY_MEDIUM = "M";
-    protected final static String ITEM_PRIORITY_LOW = "L";
+    protected static final String ITEMS_LIST = "items_list";
+    protected static final String ITEMS_ID = "item_id";
+    protected static final String ITEM = "item";
+    protected static final String ITEM_DETAIL = "item_detail";
+    protected static final String ITEM_DURATION = "duration";
+    protected static final String ITEM_DATETIME = "date";
+    protected static final String ITEM_DONE_COLUMN = "item_done";
+    protected static final String ITEM_DONE = "Y";
+    protected static final String ITEM_NOT_DONE = "N";
+    protected static final String ITEM_PRIORITY = "priority";
+    protected static final String ITEM_PRIORITY_HIGH = "H";
+    protected static final String ITEM_PRIORITY_MEDIUM = "M";
+    protected static final String ITEM_PRIORITY_LOW = "L";
 
 
 
     // SQLite Queries - including prepared statement queries
-    protected final static String ITEM_INSERT_QUERY = "INSERT INTO items_list VALUES (NULL, ?, ?," +
+    protected static final String ITEM_INSERT_QUERY = "INSERT INTO items_list VALUES (NULL, ?, ?," +
             " ?, TIME (?), DATETIME (?), ?, ?);";
     // datetime format = YYYY-MM-YY HH:YY:SS
 
-    protected final static String ITEMS_NOT_DONE_GET_QUERY = "SELECT * FROM items_list WHERE title_id = ?" +
+    protected static final String ITEMS_NOT_DONE_GET_QUERY = "SELECT * FROM items_list WHERE title_id = ?" +
             " AND item_done = 'N' ORDER BY items_list.item COLLATE NOCASE ASC;";
 
-    protected final static String ITEMS_DONE_GET_QUERY = "SELECT * FROM items_list WHERE title_id = ?" +
+    protected static final String ITEMS_DONE_GET_QUERY = "SELECT * FROM items_list WHERE title_id = ?" +
             " AND item_done = 'Y' ORDER BY items_list.item COLLATE NOCASE ASC;";
 
-    protected final static String TITLES_GET_QUERY = "SELECT * FROM titles_list ORDER BY titles_list.title COLLATE NOCASE ASC";
+    protected static final String TITLES_GET_QUERY = "SELECT * FROM titles_list ORDER BY titles_list.title COLLATE NOCASE ASC";
 
-    protected final static String TITLES_GET_TITLE_ID = "SELECT title_id FROM titles_list WHERE title_id = " +
+    protected static final String TITLES_GET_TITLE_ID = "SELECT title_id FROM titles_list WHERE title_id = " +
             "(SELECT title_id FROM titles_list WHERE titles_list.title = ?)";
 
-    protected final static String TITLES_GET_TITLE = "SELECT title FROM titles_list WHERE titles_list.title_id = ?";
+    protected static final String TITLES_GET_TITLE = "SELECT title FROM titles_list WHERE titles_list.title_id = ?";
 
-    protected final static String TITLES_CHECK_TITLE_NAME_EXISTS = "SELECT title FROM titles_list WHERE title = ?";
+    protected static final String TITLES_CHECK_TITLE_NAME_EXISTS = "SELECT title FROM titles_list WHERE title = ?";
 
-    protected final static String TITLE_DELETE_QUERY = "DELETE FROM titles_list WHERE title_id = ?;";
+    protected static final String TITLE_DELETE_QUERY = "DELETE FROM titles_list WHERE title_id = ?;";
 
-    protected final static String ITEMS_DELETE_ALL_SINGLE_TITLE = "DELETE FROM items_list WHERE " +
+    protected static final String ITEMS_DELETE_ALL_SINGLE_TITLE = "DELETE FROM items_list WHERE " +
             "title_id = (SELECT title_id FROM titles_list WHERE titles_list.title = ?);";
 
-    protected final static String ITEM_DELETE_SINGLE = "DELETE FROM items_list WHERE items_list.item_id = ?;";
+    protected static final String ITEM_DELETE_SINGLE = "DELETE FROM items_list WHERE items_list.item_id = ?;";
 
-    protected final static String ITEM_GET_SINGLE = "SELECT * FROM items_list WHERE items_list.item_id = ?";
+    protected static final String ITEM_GET_SINGLE = "SELECT * FROM items_list WHERE items_list.item_id = ?";
 
-    protected final static String ITEM_UPDATE = "UPDATE items_list SET item = ?, item_detail = ?, duration = ?, priority = ? WHERE item_id = ?;";
+    protected static final String ITEM_UPDATE = "UPDATE items_list SET item = ?, item_detail = ?, duration = ?, priority = ? WHERE item_id = ?;";
 
-    protected final static String ITEM_UPDATE_ITEM_DONE = "UPDATE items_list SET item_done = ? WHERE item_id = ?;";
+    protected static final String ITEM_UPDATE_ITEM_DONE = "UPDATE items_list SET item_done = ? WHERE item_id = ?;";
 
     // RequestCode onActivityResult - HomeFragment NEW LIST
-    protected final static int NEW_ITEMS_REQUEST_CODE = 30;
+    protected static final int NEW_ITEMS_REQUEST_CODE = 30;
 
     // RequestCode onActivityResult - HomeFragment EDIT LIST
-    protected final static String EDIT_LIST = "com.treefrogapps.TaDo.edit_list";
-    protected final static String EDIT_LIST_ACTIVITY_TITLE = "EDIT LIST";
+    protected static final String EDIT_LIST = "com.treefrogapps.TaDo.edit_list";
+    protected static final String EDIT_LIST_ACTIVITY_TITLE = "EDIT LIST";
 
 
 }
--- app/src/main/java/com/treefrogapps/TaDo/SyncFragment.java
+++ app/src/main/java/com/treefrogapps/TaDo/SyncFragment.java
@@ -1,15 +1,61 @@
 package com.treefrogapps.TaDo;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.query.Filters;
+import com.google.android.gms.drive.query.Query;
+import com.google.android.gms.drive.query.SearchableField;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
-public class SyncFragment extends Fragment {
+public class SyncFragment extends Fragment implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    private View rootView;
+    private SharedPreferences sharedPreferences;
+    private DBHelper dbHelper;
+
+    private GoogleApiClient mGoogleApiClient;
+    private DriveId driveId;
+    private String driveFileId;
+    private Button mButtonUpload;
+    private Button mButtonDownload;
+    private Button mButtonDelete;
+
+    private static int connectionType = 0;
+    // 1 = upload / amend file / overwrite file
+    // 2 = download
+    // 3 = delete
 
     public SyncFragment() {
         // Required empty public constructor
@@ -18,18 +64,428 @@ public class SyncFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_sync, container, false);
+        rootView = inflater.inflate(R.layout.fragment_sync, container, false);
+        return rootView;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        sharedPreferences = getActivity().getSharedPreferences(Constants.TADO_PREFERENCES, Context.MODE_PRIVATE);
+        dbHelper = new DBHelper(getActivity());
+
+        mButtonUpload = (Button) rootView.findViewById(R.id.mButtonUpload);
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                connectionType = 1;
+
+                // TODO ONE - 2 - attempt to connect
+                buildConnection();
+                mGoogleApiClient.connect();
+            }
+        });
+
+        mButtonDownload = (Button) rootView.findViewById(R.id.mButtonDownload);
+        mButtonDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                connectionType = 2;
+
+                // TODO TWO - 2 - attempt to connect
+                buildConnection();
+                mGoogleApiClient.connect();
+            }
+        });
+
+        mButtonDelete = (Button) rootView.findViewById(R.id.mButtonDelete);
+        mButtonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                connectionType = 3;
+
+                // TODO ONE THREE - 2 - attempt to connect
+                buildConnection();
+                mGoogleApiClient.connect();
+            }
+        });
+
+    } // End of onActivityCreated
+
+    public void buildConnection() {
+        // TODO ONE TWO THREE 1.
+        // build the api client
+        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addScope(Drive.SCOPE_APPFOLDER)
+                .addConnectionCallbacks(SyncFragment.this)
+                .addOnConnectionFailedListener(SyncFragment.this)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // TODO ONE 3 - connected, now methods executed based on int value
+        if (connectionType == 1) {
+            uploadFile();
+        } else if (connectionType == 2) {
+            downloadFile();
+        } else if (connectionType == 3) {
+            deleteTheFile();
+        }
+
+    }
+
+    // TODO ONE - 4
+    private void uploadFile() {
+
+        // search for existing file
+        Query query = new Query.Builder()
+                .addFilter(Filters.eq(SearchableField.TITLE, "main_database.db"))
+                .addFilter(Filters.eq(SearchableField.MIME_TYPE, "application/x-sqlite3"))
+                .build();
+
+        // execute query to look if file already exists
+        Drive.DriveApi.query(mGoogleApiClient, query).setResultCallback(searchFileCallBack);
+
+    }
+
+    // TODO TWO - 4
+    private void downloadFile() {
+        // search for existing file
+        Query query = new Query.Builder()
+                .addFilter(Filters.eq(SearchableField.TITLE, "main_database.db"))
+                .addFilter(Filters.eq(SearchableField.MIME_TYPE, "application/x-sqlite3"))
+                .build();
+
+        // execute query to look if file already exists
+        Drive.DriveApi.query(mGoogleApiClient, query).setResultCallback(searchFileCallBack);
+    }
+
+    // TODO THREE - 4
+    private void deleteTheFile() {
+        // search for existing file
+        Query query = new Query.Builder()
+                .addFilter(Filters.eq(SearchableField.TITLE, "main_database.db"))
+                .addFilter(Filters.eq(SearchableField.MIME_TYPE, "application/x-sqlite3"))
+                .build();
+
+        // execute query to look if file already exists
+        Drive.DriveApi.query(mGoogleApiClient, query).setResultCallback(searchFileCallBack);
+    }
+
+    // TODO ONE THREE - 4a - check if file exists and either overwrite using its unique DriveId (file id) or create new file if it doesn't exist
+    private ResultCallback<DriveApi.MetadataBufferResult> searchFileCallBack = new ResultCallback<DriveApi.MetadataBufferResult>() {
+        @Override
+        public void onResult(DriveApi.MetadataBufferResult metadataBufferResult) {
+            if (!metadataBufferResult.getStatus().isSuccess()) {
+                Toast.makeText(getActivity(), "Error searching", Toast.LENGTH_SHORT).show();
+
+            } else if (metadataBufferResult.getMetadataBuffer().getCount() > 0) {
+
+                if (metadataBufferResult.getMetadataBuffer().get(0).getMimeType().equals("application/x-sqlite3")) {
+
+                    // TODO ONE THREE = 4b - a result is returned meaning the file exists and we get its unique ID
+                    driveId = metadataBufferResult.getMetadataBuffer().get(0).getDriveId();
+                    Log.e("DRIVE ID FOUND", driveId.encodeToString());
+                    // start async task to edit / overwrite contents of file
+                    DriveFile driveFile = Drive.DriveApi.getFile(mGoogleApiClient, driveId);
+
+                    metadataBufferResult.release();
+
+                    if (connectionType == 1) {
+                        new EditContentsAsyncTask(getActivity()).execute(driveFile);
+                    } else if (connectionType == 2) {
+                        new RetrieveFileAsyncTask(getActivity()).execute(driveFile);
+                    } else if (connectionType == 3) {
+                        new DeleteFileAsyncTask(getActivity()).execute(driveFile);
+                    }
+                }
+
+
+            } else if (connectionType == 1) {
+
+                metadataBufferResult.release();
+                // TODO ONE 4b
+                // new drive contents - file doesn't exist yet
+                Drive.DriveApi.newDriveContents(mGoogleApiClient).setResultCallback(driveContentsCallback);
+            } else {
+                metadataBufferResult.release();
+                Toast.makeText(getActivity(), "NO MATCHING ENTRIES TO main_database.db", Toast.LENGTH_SHORT).show();
+                mGoogleApiClient.disconnect();
+            }
+        }
+    };
+
+
+    // TODO ONE 4b - get drive contents callback
+    private ResultCallback<DriveApi.DriveContentsResult> driveContentsCallback = new ResultCallback<DriveApi.DriveContentsResult>() {
+        @Override
+        public void onResult(DriveApi.DriveContentsResult driveContentsResult) {
+
+            if (!driveContentsResult.getStatus().isSuccess()) {
+                Toast.makeText(getActivity(), "Error getting drive contents", Toast.LENGTH_SHORT).show();
+                mGoogleApiClient.disconnect();
+
+            } else {
+                // Get an outputStream for the contents
+                OutputStream outputStream = driveContentsResult.getDriveContents().getOutputStream();
+
+                try {
+                    FileInputStream inputStream = new FileInputStream(getActivity().getDatabasePath("main_database.db"));
+
+                    // write the input stream into the drive output stream
+                    byte[] byteBuffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(byteBuffer)) > 0) {
+                        outputStream.write(byteBuffer, 0, length);
+                    }
+                    outputStream.flush();
+                    outputStream.close();
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // set its title and mime type
+                MetadataChangeSet metaDataChangeSet = new MetadataChangeSet.Builder()
+                        .setMimeType("application/x-sqlite3").setTitle("main_database.db").build();
+
+                // TODO ONE - 4c
+                Drive.DriveApi.getAppFolder(mGoogleApiClient)
+                        .createFile(mGoogleApiClient, metaDataChangeSet, driveContentsResult.getDriveContents())
+                        .setResultCallback(fileUploadCallBack);
+            }
+        }
+    };
+
+
+    // TODO ONE - 4c - check uploaded ok
+    private ResultCallback<DriveFolder.DriveFileResult> fileUploadCallBack = new ResultCallback<DriveFolder.DriveFileResult>() {
+        @Override
+        public void onResult(DriveFolder.DriveFileResult driveFileResult) {
+
+            if (!driveFileResult.getStatus().isSuccess()) {
+                Toast.makeText(getActivity(), "Error creating file", Toast.LENGTH_SHORT).show();
+
+            } else {
+                driveFileId = driveFileResult.getDriveFile().getDriveId().encodeToString();
+                Log.e("DRIVE FILE ID ", driveFileId);
+                Toast.makeText(getActivity(), "File uploaded successfully", Toast.LENGTH_SHORT).show();
+            }
+
+            mGoogleApiClient.disconnect();
+        }
+    };
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // TODO ONE 2b
+        if (connectionResult.hasResolution()) {
+
+            try {
+                connectionResult.startResolutionForResult(getActivity(), Constants.REQUEST_CODE_RESOLUTION);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+
+                Toast.makeText(getActivity(), "Unable to complete connection to GDrive", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.e("CALLED", "OnActivity Result");
+
+        switch (requestCode) {
+            // TODO ONE - 2c
+            case Constants.REQUEST_CODE_RESOLUTION:
+                if (resultCode == Activity.RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+        }
+    }
+
+    //Todo ONE - 4b - edit contents using async task
+    public class EditContentsAsyncTask extends AsyncTask<DriveFile, Void, Boolean> {
+
+        public EditContentsAsyncTask(Context context) {
+        }
+
+        @Override
+        protected Boolean doInBackground(DriveFile[] driveFile) {
+
+            DriveFile fileToEdit = driveFile[0];
+
+            try {
+                DriveApi.DriveContentsResult driveContentsResult =
+                        fileToEdit.open(mGoogleApiClient, DriveFile.MODE_WRITE_ONLY, null).await();
+
+                if (!driveContentsResult.getStatus().isSuccess()) {
+                    return false;
+                }
+
+                DriveContents driveContents = driveContentsResult.getDriveContents();
+                OutputStream outputStream = driveContents.getOutputStream();
+
+                FileInputStream inputStream = new FileInputStream(getActivity().getDatabasePath("main_database.db"));
+
+                byte[] byteBuffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(byteBuffer)) > 0) {
+                    outputStream.write(byteBuffer, 0, length);
+                }
+                outputStream.flush();
+                outputStream.close();
+                inputStream.close();
+
+                com.google.android.gms.common.api.Status status =
+                        driveContents.commit(mGoogleApiClient, null).await();
+
+                return status.getStatus().isSuccess();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            if (!result) {
+                Toast.makeText(getActivity(), "Error whilst editing contents", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getActivity(), "Successfully edited contents", Toast.LENGTH_SHORT).show();
+            }
+
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    // TODO TWO - 4b - asynctask to retrieve file and write into database file
+    public class RetrieveFileAsyncTask extends AsyncTask<DriveFile, Void, Boolean> {
+
+        public RetrieveFileAsyncTask(Context context) {
+        }
+
+        @Override
+        protected Boolean doInBackground(DriveFile[] driveFile) {
+
+            DriveFile fileToDownload = driveFile[0];
+
+            try {
+                DriveApi.DriveContentsResult driveContentsResult
+                        = fileToDownload.open(mGoogleApiClient, DriveFile.MODE_READ_ONLY, null).await();
+
+                if (!driveContentsResult.getStatus().isSuccess()) {
+                    return false;
+                }
+
+                File databaseFile;
+
+                if (getActivity().getDatabasePath("main_database.db").exists()) {
+                    databaseFile = getActivity().getDatabasePath("main_database.db");
+                } else {
+                    return false;
+                }
+
+                OutputStream outputStream = new FileOutputStream(databaseFile);
+
+                DriveContents driveContents = driveContentsResult.getDriveContents();
+                InputStream inputStream = driveContents.getInputStream();
+
+                byte[] byteBuffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(byteBuffer)) > 0) {
+                    outputStream.write(byteBuffer, 0, length);
+                }
+                outputStream.flush();
+                outputStream.close();
+                inputStream.close();
+
+                return true;
+
+            } catch (IOException eIO) {
+                eIO.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            if (!result) {
+                Toast.makeText(getActivity(), "Error whilst writing database locally", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getActivity(), "Successfully downloaded database", Toast.LENGTH_SHORT).show();
+            }
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+
+    // TODO THREE - 4b  - asynctask to delete the file from g drive
+    public class DeleteFileAsyncTask extends AsyncTask<DriveFile, Void, Boolean> {
+
+        public DeleteFileAsyncTask(Context context) {
+        }
+
+        @Override
+        protected Boolean doInBackground(DriveFile[] driveFile) {
+
+            com.google.android.gms.common.api.Status deleteStatus
+                    = driveFile[0].delete(mGoogleApiClient).await();
+
+            return deleteStatus.getStatus().isSuccess();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+
+            if (!result) {
+                Toast.makeText(getActivity(), "Error whilst deleting", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getActivity(), "Successfully deleted File", Toast.LENGTH_SHORT).show();
+            }
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
     }
 }
